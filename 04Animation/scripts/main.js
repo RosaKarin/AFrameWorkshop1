e11b861c9e3e46c8bd060072aa22a7af3b45677e
RosaKarin <50404764+RosaKarin@users.noreply.github.com>
2021-09-20 15:34:21 +0200

    Update main.js

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -3,7 +3,7 @@
  var myOtherBox = document.getElementById('myOtherBox');
 
  function spin(){
-	myOtherBox.object3D.rotation.yx += rotationSpeed;
+	myOtherBox.object3D.rotation.y += rotationSpeed;
  	console.log(myOtherBox.object3D.rotation.yx);
  }
 
